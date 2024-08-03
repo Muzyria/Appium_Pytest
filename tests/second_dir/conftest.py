84ca70b44298053052232921b3f9fc347f9934d0
FILA <fila090580@gmail.com>
2024-08-03 18:56:33 +0300

    first commit

---
+++ tests/second_dir/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+
+@pytest.fixture
+def driver():
+    print()
+    print("START conftest 3")
+    yield "__SECOND_CONFTEST__"
+    print()
+    print("finish conftest 3")
+
---
+++ tests/second_dir/test_example_second.py
@@ -0,0 +1,5 @@
+
+
+def test_second(driver):
+    print("second test")
+    print(driver)
